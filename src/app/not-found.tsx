22b46e11e49e9ba398faea95856a51c65f9ac401
Huma-Mohsin <160951214+Huma-Mohsin@users.noreply.github.com>
2024-10-28 06:47:29 +0500

    Initial commit from Create Next App

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+function ErrorHandling(){
+    return(
+        <div>
+            <h1>Sorry, The Page Did Not Exist...</h1>
+            <p>Try with relevant search...</p>
+        </div>
+    )
+}
